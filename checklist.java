97d6162ea3a5071e994a9119e404b247afb02aae
Evan Suslovich <evan.suslovich@lincolnps.org>
2020-12-19 21:06:56 -0500

    Working on this

--- checklist.java
+++ checklist.java
@@ -1,20 +1,14 @@
-import java.util.ArrayList; // arraylist library
+import java.util.LinkedList;
 import java.util.Scanner; 
 
 public class checklist{
 
     private int counter = 0;
-    private ArrayList<task> tasks; 
-    public static void main(String[] args){
-        // I am going to make an arraylist called tasks 
-        // The arraylist is going to hold a class called "task" 
-        //"task" is going to have a paramter with the assignment in a string 
-        new checklist(); 
-    }
+    private LinkedList<task> tasks; 
+    
 
     public checklist(){
-        makeNewTask(); 
-        printCheckList();
+        //makeNewTask(); 
     }
 
     public void makeNewTask(){
@@ -24,20 +18,24 @@ public class checklist{
         
         counter++;
         task t1 = new task(taskString, counter);
-        tasks.add(t1);
+        addTask(t1); 
     }
+    public void addTask(task t){
+        tasks.add(t); 
+    }
+
+
+
+
 
     
-    public void printCheckList(){
-        System.out.println("CHECKLIST:");
-        System.out.println("Number of assingments: " + counter);
-
-        for(int i = 0; i < tasks.size(); i++){
-            System.out.println("Assingment number: " + tasks.get(i).getNumber()); 
-            System.out.println("Assingment task: " + tasks.get(i).getTask()); 
-
-            System.out.println(); 
-        }
+    public static void main(String[] args){
+        // I am going to make an arraylist called tasks 
+        // The arraylist is going to hold a class called "task" 
+        //"task" is going to have a paramter with the assignment in a string 
+        new checklist(); 
     }
+
+
     
 }
