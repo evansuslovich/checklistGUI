de3d256f7dbd99da66b8ce16522c957c561c2b65
Evan Suslovich <evan.suslovich@lincolnps.org>
2020-12-20 22:48:52 -0500

    I had to make create a private linked list to get it to work

--- checklist.java
+++ checklist.java
@@ -3,17 +3,12 @@ import java.util.Scanner;
 
 public class checklist{
 
-    static LinkedList<task> tasks = new LinkedList<task>();
-
-     /*
+    private LinkedList<task> tasks = new LinkedList<task>();
     private int counter = 0;
-    private LinkedList<task> tasks; 
     
-
    public checklist(){
-        task t1 = new task("Hello", 1); 
-        tasks.add(t1); 
-        //printTasks(); 
+        makeNewTask(); 
+        printTasks(); 
     }
 
     public void printTasks(){
@@ -28,11 +23,10 @@ public class checklist{
         String taskString = scan.nextLine();  
         counter++; 
 
-        System.out.println(new task(taskString,counter));
-
-        //tasks.add(new task(taskString,counter));
+        tasks.add(new task(taskString,counter));
+        scan.close(); 
     }
-*/ 
+
 
 
 
@@ -42,30 +36,14 @@ public class checklist{
         // I am going to make an arraylist called tasks 
         // The arraylist is going to hold a class called "task" 
         //"task" is going to have a paramter with the assignment in a string 
-
-        int counter = 0; 
-        
-        Scanner scan = new Scanner(System.in); 
-        System.out.println("Enter a task"); 
-        String taskString = scan.nextLine(); 
-        counter++; 
-        
-        tasks.add(new task(taskString,counter));
-        tasks.add(new task("Eat", 2)); 
-        tasks.add(new task("Shower", 3));
-        
-
-
-        for(int i = 0; i < tasks.size(); i++){
-            System.out.println(tasks.get(i)); 
+        new checklist(); 
         }
 
 
 
 
 
-    }
+}
 
 
     
-}
