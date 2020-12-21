8081b45d68fbfe9dc8b1fcfdec1f1e1ef81dace2
Evan Suslovich <evan.suslovich@lincolnps.org>
2020-12-20 23:05:59 -0500

    I learned that most of my problems are becasue I have been making objects inside of methods. The problem is when I call the method, they always give me errors

--- checklist.java
+++ checklist.java
@@ -4,31 +4,37 @@ import java.util.Scanner;
 public class checklist{
 
     private LinkedList<task> tasks = new LinkedList<task>();
+    private Scanner scan = new Scanner(System.in);
+
     private int counter = 0;
     
    public checklist(){
+        makeNewTask();
         makeNewTask(); 
+        
         printTasks(); 
     }
 
     public void printTasks(){
+        System.out.println(); 
         for(int i = 0; i < tasks.size(); i++){
             System.out.println(tasks.get(i));
         }
     }
 
     public void makeNewTask(){
-        Scanner scan = new Scanner(System.in); 
+        String taskString = "";
+
         System.out.println("Enter a task");
-        String taskString = scan.nextLine();  
+        taskString = scan.nextLine(); 
+        
+    
         counter++; 
 
+
         tasks.add(new task(taskString,counter));
-        scan.close(); 
+
     }
-
-
-
 
 
 
