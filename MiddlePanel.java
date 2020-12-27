59c549ee464bb58cef62ee40b39702ce6539e2b2
evansuslovich <evan.suslovich@lincolnps.org>
2020-12-26 23:06:42 -0500

    More

--- MiddlePanel.java
+++ MiddlePanel.java
@@ -1,10 +1,17 @@
 import javax.swing.*; 
 import java.awt.*; 
+import java.util.LinkedList;
+
 
 public class MiddlePanel extends JPanel{
     private static final long serialVersionUID = 1L; 
 
+    static LinkedList<task> tasks = new LinkedList<task>();
+
     public MiddlePanel(){
         setBackground(new Color(255,69,0)); 
+        setLayout(new SpringLayout()); 
+        
+        
     }
 }
