2732d8a3a10abb8b5d96c84364f565983dc8e264
evansuslovich <evan.suslovich@lincolnps.org>
2020-12-26 21:11:32 -0500

    Add

---
+++ TopPanel.java
@@ -0,0 +1,10 @@
+import javax.swing
+
+public class TopPanel extends JPanel{
+    private static final long serialVersionUID = 1L; 
+
+    public TopPanel(){
+        this.setBackground(new Color(255,165,0)); 
+        
+    }
+}
--- taskGUI.java
+++ taskGUI.java
@@ -1,12 +1,26 @@
 import javax.swing.*; 
+import java.awt.*; 
 
 
 public class taskGUI{
 
     public taskGUI(){
         JFrame frame = new JFrame(); 
-        frame.
+        frame.setSize(200,600); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout()); 
+        frame.getRootPane().setBorder(BorderFactory.createMatteBorder(5,5,5,5,Color.WHITE)); 
 
+
+        JPanel middlePanel = new JPanel(); 
+        middlePanel.setBackground(new Color(255,165,0)); 
+
+        frame.add(middlePanel, BorderLayout.CENTER); 
+
+
+
+
+        frame.setVisible(true);
     }
     public static void main(String[] args){
         // GUI 
