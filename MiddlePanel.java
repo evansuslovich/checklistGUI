f9b8ed595102b56417e9f75b27bfafb19d6e5461
evansuslovich <evan.suslovich@lincolnps.org>
2020-12-26 22:43:35 -0500

    I don't know if I am going to add the linkedlist in the middlepanel. I am having lots of trouble understanding how I am going to access everyting

---
+++ MiddlePanel.java
@@ -0,0 +1,10 @@
+import javax.swing.*; 
+import java.awt.*; 
+
+public class MiddlePanel extends JPanel{
+    private static final long serialVersionUID = 1L; 
+
+    public MiddlePanel(){
+        setBackground(new Color(255,69,0)); 
+    }
+}
