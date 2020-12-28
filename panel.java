a9657e8f64c6f963e9508847d7f3f867f68390c7
evansuslovich <evan.suslovich@lincolnps.org>
2020-12-28 12:49:58 -0500

    Combined the TopPanel and MiddlePanel classes

---
+++ panel.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+//import javax.swing.text.FlowView;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.LinkedList;
+
+public class Panel extends JPanel{
+    private static final long serialVersionUID = 1L; 
+
+    public Panel(){
+
+        // 
+
+
+
+    }
+}
