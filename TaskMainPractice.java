dd79ee5f2e3a8ddddfbdb021da0ab7940a153799
evansuslovich <evan.suslovich@lincolnps.org>
2020-12-28 13:41:34 -0500

    It looks like everything is working properly. I made three objects and added them

---
+++ TaskMainPractice.java
@@ -0,0 +1,29 @@
+import java.awt.*; 
+import javax.swing.*; 
+import java.util.LinkedList; 
+
+public class TaskMainPractice{
+    private LinkedList<task> tasks = new LinkedList<task>();
+    public TaskMainPractice(){
+        JFrame frame = new JFrame();
+        frame.setSize(200,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+
+        tasks.add(new task("Run", 1, tasks));  
+        tasks.add(new task("Cook", 2, tasks)); 
+        tasks.add(new task("Eat", 3, tasks)); 
+
+        frame.add(tasks.get(0)); 
+        frame.add(tasks.get(1)); 
+        frame.add(tasks.get(2)); 
+
+        
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args){
+        new TaskMainPractice();
+    }
+}
