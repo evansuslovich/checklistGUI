a6f607d81d8712704b84ed4c0f01ab8926be4fc9
evansuslovich <evan.suslovich@lincolnps.org>
2020-12-28 12:54:20 -0500

    Removed the middle and top panels. I also deleted the layout.

--- taskGUI.java
+++ taskGUI.java
@@ -1,22 +1,15 @@
 import javax.swing.*; 
-import java.awt.*; 
-
 public class taskGUI{
-    static MiddlePanel middlePanel = new MiddlePanel();
-    static TopPanel topPanel = new TopPanel(); 
+    
+    static Panel panel = new Panel(); 
 
     public taskGUI(){
         JFrame frame = new JFrame(); 
         frame.setSize(200,600); 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout()); 
-        frame.getRootPane().setBorder(BorderFactory.createMatteBorder(5,5,5,5,Color.WHITE)); 
+        //frame.getRootPane().setBorder(BorderFactory.createMatteBorder(5,5,5,5,Color.WHITE)); 
 
-        
-
-        frame.add(topPanel, BorderLayout.NORTH);
-        frame.add(middlePanel, BorderLayout.CENTER);
-
+        frame.add(panel); 
         frame.setVisible(true);
     }
     public static void main(String[] args){
