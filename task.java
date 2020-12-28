0ed6a88b842f1d9f8a4735374f0d88a4228f203f
evansuslovich <evan.suslovich@lincolnps.org>
2020-12-27 22:29:10 -0500

    New task can be created. I made the simple error of not setting the parameters to the private variables.

--- task.java
+++ task.java
@@ -3,29 +3,39 @@ import java.awt.*;
 import java.awt.event.*; 
 
 
-public class task extends JPanel{
-    private static final long serialVersionUID = 1L; 
+public class task{
+    //private static final long serialVersionUID = 1L; 
+
     private String task; 
     private int number; 
 
-   
     JButton checkMark = new JButton(); 
-    
+
+    JPanel panel = new JPanel(); 
+
     public task(String task, int number){
-        setLayout(new FlowLayout());
+        this.task = task; 
+        this.number = number; 
+        
+        panel.setLayout(new FlowLayout());
 
         JLabel label = new JLabel("Task " + number + ": " + task); 
         label.setFont(new Font("Serif",Font.BOLD, 12));
-        
         ImageIcon icon = new ImageIcon("check.png");
         JButton button = new JButton(icon); 
 
+        panel.add(label); 
+        panel.add(button); 
+
         button.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-
+                
             }
         });
+    }
 
+    public String toString(){
+        return "Task " + number + ": " + task; 
     }
 
     public int getLength(){
